04c96d7add00516e6e7b843afa83ac629df98a14
David Felce <davefelce@gmail.com>
2023-02-06 16:56:11 +0000

    added types to tests

--- tests/test_input_one.py
+++ tests/test_input_one.py
@@ -4,27 +4,27 @@ import pytest
 from certino_test.input_one import calc_sum
 
 
-def test_calc_sum_with_fake(fake_numbers: Generator):
+def test_calc_sum_with_fake(fake_numbers: Generator) -> None:
     calculated_sum = calc_sum(numbers=fake_numbers, required_value=2020, n_elements=2)
     assert calculated_sum == 514579
 
 
-def test_calc_sum(numbers: Generator):
+def test_calc_sum(numbers: Generator) -> None:
     calculated_sum = calc_sum(numbers=numbers, required_value=2020, n_elements=2)
     assert calculated_sum == 1013211
 
 
-def test_calc_sum_raises(numbers: Generator):
+def test_calc_sum_raises(numbers: Generator) -> None:
     with pytest.raises(ValueError):
         calc_sum(numbers=numbers, required_value=1, n_elements=2)
 
 
-def test_calc_sum_with_fake_and_three_elements(fake_numbers: Generator):
+def test_calc_sum_with_fake_and_three_elements(fake_numbers: Generator) -> None:
     calculated_sum = calc_sum(numbers=fake_numbers, required_value=2020, n_elements=3)
     assert calculated_sum == 241861950
 
 
-def test_calc_sum_and_three_elements(numbers: Generator):
+def test_calc_sum_and_three_elements(numbers: Generator) -> None:
     calculated_sum = calc_sum(numbers=numbers, required_value=2020, n_elements=3)
     assert calculated_sum == 13891280
 
--- tests/test_input_two.py
+++ tests/test_input_two.py
@@ -3,21 +3,21 @@ from typing import Generator
 from certino_test.input_two import calc_valid_positions, calc_valid_strings
 
 
-def test_calc_valid_strings_with_fake(input_two_fake_lines: Generator):
+def test_calc_valid_strings_with_fake(input_two_fake_lines: Generator) -> None:
     valid_strings = calc_valid_strings(input_two_fake_lines)
     assert valid_strings == 2
 
 
-def test_calc_valid_strings(input_two_lines: Generator):
+def test_calc_valid_strings(input_two_lines: Generator) -> None:
     valid_strings = calc_valid_strings(input_two_lines)
     assert valid_strings == 666
 
 
-def test_calc_valid_positions_with_fake(input_two_fake_lines: Generator):
+def test_calc_valid_positions_with_fake(input_two_fake_lines: Generator) -> None:
     valid_strings = calc_valid_positions(input_two_fake_lines)
     assert valid_strings == 1
 
 
-def test_calc_valid_positions(input_two_lines: Generator):
+def test_calc_valid_positions(input_two_lines: Generator) -> None:
     valid_strings = calc_valid_positions(input_two_lines)
     assert valid_strings == 29
